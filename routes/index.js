7dc28e8662460797b5f5e3fc2a5774e80f3fe76d
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-06-11 22:52:06 -0600

    adding Tina to home page route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const routes = require('express').Router()
 const lesson1Controller = require('../controllers/lesson1')
 
-routes.get('/', lesson1Controller.helloRoute)
+routes.get('/', lesson1Controller.helloTinaRoute)
 routes.get('/Aaron', lesson1Controller.helloAaronRoute)
-routes.get('/Tina', lesson1Controller.helloTinaRoute)
 routes.get('/Braedan', lesson1Controller.helloBraedanRoute)
 routes.get('/Noah', lesson1Controller.helloNoahRoute)
 
